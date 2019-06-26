1bd56863f4b27bb46db23376f18e2c064d6bebba
Alan Tsai <alanltsai@gmail.com>
2019-06-25 21:15:53 -0700

    Added index.html, logic.js

---
+++ Working/logic.js
@@ -0,0 +1,49 @@
+d3.csv("player_list.csv", function(error, data) {
+    if (error) throw error;
+
+    console.log(data);
+    var select = d3.select("#player").append("select");
+    select.selectAll("option")
+      .data(data)
+      .enter()
+      .append("option")
+      .attr("value", function (d) {
+        return d.Player_Name;
+      })
+      .text(function (d) { return d.Player_Name; });
+});
+
+d3.csv("seasons.csv", function(error, data) {
+    if (error) throw error;
+
+    console.log(data);
+    var select = d3.select("#season").append("select");
+    select.selectAll("option")
+      .data(data)
+      .enter()
+      .append("option")
+      .attr("value", function (d) {
+        return d.Season;
+      })
+      .text(function (d) { return d.Season; });
+});
+
+var button = d3.select("#shotPlot")
+
+
+
+function handleClick() {
+  console.log("A button was clicked");
+  var playerField = d3.select("#player").select("select");
+  var selectedPlayer = playerField.property("value");
+  console.log(`${selectedPlayer} was selected`); // Everytime button is clicked, log in console
+  // console.log(playerField);
+  // Add event listener
+  // console.log(d3.event.target); // Logs object of ID
+  var seasonField = d3.select("#season").select("select");
+  var selectedSeason = seasonField.property("value");
+  console.log(`${selectedSeason} was selected`);
+}
+
+// Attach event to handler function
+button.on("click", handleClick); // Call on function when event "click" occurs on <button>
